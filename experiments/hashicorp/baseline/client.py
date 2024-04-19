da8d1fd716036ff47c016470d5c9d6af27fbab3c
Alejandro Ciuba <alejandrociuba@gmail.com>
2024-04-19 14:35:57 -0400

    Tested experiments on my laptop

--- experiments/hashicorp/baseline/client.py
+++ experiments/hashicorp/baseline/client.py
@@ -35,7 +35,7 @@ def main(args: argparse.Namespace):
     print(f"RESULT | MESSAGES SENT/REQUESTED: {args.data}")
     print(f"RESULT | AVG COMMIT TIME: {time}")
     print(f"RESULT | AVG CPU UTIL: {avg_cpu}%")
-    print(f"RESULT | MAX MEM UTIL: {ps_mem / (2**10)} kiB")
+    print(f"RESULT | MAX MEM UTIL: {ps_mem / (2**10)} KiB")
     print(f"RESULT | END LOG SIZE: {log_size} B")
 
 
--- experiments/hashicorp/baseline/client.sh
+++ experiments/hashicorp/baseline/client.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# python client.py -s $LEADER -d 10000
+python client.py -s $LEADER -d 10000
 
-python client.py -s $LEADER -d 10000 -k
+# python client.py -s $LEADER -d 10000 -k

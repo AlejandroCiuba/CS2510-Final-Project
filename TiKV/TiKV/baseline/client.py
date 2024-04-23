dcbf3a9eb703fa7fb8cd8a88bcd7a2471cf1bbe7
Alejandro <alejandrociuba@gmail.com>
2024-04-23 01:57:00 -0400

    Working on updating TiKV to pipeline

---
+++ TiKV/TiKV/baseline/client.py
@@ -0,0 +1,63 @@
+from tikv_client import RawClient
+from tqdm import tqdm
+
+import argparse
+import time
+
+
+def main(args: argparse.Namespace):
+
+    TEST = {f"{i:d}": f"{i:d}" for i in range(args.data)}
+
+    # Time sleep
+    # time.sleep(30)
+
+    # Connect to the TiKV cluster
+    client = RawClient.connect(["127.0.0.1:2379"])
+
+    for key in tqdm(TEST):
+        client.put(b"%s" % key.encode(), b"%s" % TEST[key].encode())
+
+    for key in tqdm(TEST):
+        assert TEST[key] == f"{client.get(b"%s" % key.encode()).decode()}"
+        print(f"{client.get(b"%s" % key.encode()).decode()}")
+
+
+def add_args(parser: argparse.ArgumentParser):
+
+    parser.add_argument(
+        "-d",
+        "--data",
+        default=1_000,
+        type=int,
+        help="Number of test values to generate.\n \n",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--server",
+        nargs="+",
+        help="Server addresses with ports.\n \n",
+    )
+
+    parser.add_argument(
+        "-k",
+        "--skeleton",
+        action="store_true",
+        help="Add if it is a skeleton run. Spooky ooO!\n \n",
+    )
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        prog="client.py",
+        formatter_class=argparse.RawTextHelpFormatter,
+        description="Client class for the TiKV final project.",
+        epilog="Created by Alejandro Ciuba, alc307@pitt.edu",
+    )
+
+    add_args(parser)
+    args = parser.parse_args()
+
+    main(args=args)
---
+++ TiKV/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export TIKV_VERSION=v4.0.12
+export GOOS=linux  # only {darwin, linux} are supported
+export GOARCH=amd64 # only {amd64, arm64} are supported
+curl -O  https://tiup-mirrors.pingcap.com/tikv-$TIKV_VERSION-$GOOS-$GOARCH.tar.gz
+curl -O  https://tiup-mirrors.pingcap.com/pd-$TIKV_VERSION-$GOOS-$GOARCH.tar.gz
+tar -xzf tikv-$TIKV_VERSION-$GOOS-$GOARCH.tar.gz
+tar -xzf pd-$TIKV_VERSION-$GOOS-$GOARCH.tar.gz

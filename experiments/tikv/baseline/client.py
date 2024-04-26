f1a46bcc3ef93c0568bf79087966542ace222b10
Alejandro <alejandrociuba@gmail.com>
2024-04-25 23:11:52 -0400

    Worked on stats

--- experiments/tikv/baseline/client.py
+++ experiments/tikv/baseline/client.py
@@ -2,9 +2,7 @@ from tikv_client import RawClient
 from timer import (Timer,
                    TimerVault)
 from tqdm import tqdm
-import threading
 import argparse
-import time
 import requests
 
 # Global variables, used for monitoring requests
@@ -12,14 +10,20 @@ prometheus_url = 'http://prometheus:9090'
 
 # Make a request to Prometheus for any monitoring query
 def fetch_prometheus_query(query):
+
     try:
+
         response = requests.get(f"{prometheus_url}/api/v1/query", params={'query': query})
-        if response.status_code == 200:
-            result = response.json()
-            if result['status'] == 'success':
-                return result['data']['result']['value'] # TODO: Make this get the singular value, check the JSON format
+
+        print("HERE")
+        print(response.json())
+        
+            # if result['status'] == 'success':
+            #     return result['data']['result']['value'] # TODO: Make this get the singular value, check the JSON format
+
     except requests.exceptions.RequestException as e:
         print(f"Failed to fetch metrics: {str(e)}")
+
     return None
 
 def main(args: argparse.Namespace):
@@ -30,20 +34,24 @@ def main(args: argparse.Namespace):
     # Connect to the TiKV cluster
     client = RawClient.connect(args.servers)
 
-    for key in tqdm(TEST):
-        # Timing from here not only because of server separation, but because connection is part of the library
-        with Timer("put", TIMERVAULT):
-            client.put(b"%s" % key.encode(), b"%s" % TEST[key].encode())
-    
-    # Create queries for TiKV metrics 
-    times = TIMERVAULT.to_dict()['recordings']['total']
-    time_range = sum(times)
-    cpu_usage_query = f'avg_over_time(process_cpu_seconds_total[{time_range}s])'
-    memory_usage_query = f'avg_over_time(node_memory_MemTotal[{time_range}s])'
+    with Timer("entire", TIMERVAULT):
 
-    # Fetch CPU and memory metrics
-    avg_cpu = fetch_prometheus_query(cpu_usage_query)
-    ps_mem = fetch_prometheus_query(memory_usage_query)
+        for key in tqdm(TEST):
+
+            # Timing from here not only because of server separation, but because connection is part of the library
+            with Timer("put", TIMERVAULT):
+                client.put(b"%s" % key.encode(), b"%s" % TEST[key].encode())
+    
+    commit_times = TIMERVAULT.to_dict()['recordings']['total'][:-1]
+
+    # Create queries for TiKV metrics 
+    # total_time = TIMERVAULT.to_dict()['recordings']['total'][-1]
+    # cpu_usage_query = f'avg_over_time(process_cpu_seconds_total[{int(total_time)}s])'
+    # memory_usage_query = f'avg_over_time(node_memory_MemTotal[{int(total_time)}s])'
+
+    # # Fetch CPU and memory metrics
+    # avg_cpu = fetch_prometheus_query(cpu_usage_query)
+    # ps_mem = fetch_prometheus_query(memory_usage_query)
 
     for key in tqdm(TEST):
         assert TEST[key] == f"{client.get(b'%s' % key.encode()).decode()}"
@@ -51,9 +59,9 @@ def main(args: argparse.Namespace):
 
 
     print(f"RESULT | MESSAGES SENT/REQUESTED: {args.data}")
-    print(f"RESULT | AVG COMMIT TIME: {sum(times) / len(times)}")
-    print(f"RESULT | AVG CPU UTIL: {avg_cpu}%")
-    print(f"RESULT | MAX MEM UTIL: {ps_mem / (2**10)} KiB")
+    print(f"RESULT | AVG COMMIT TIME: {sum(commit_times) / len(commit_times)}")
+    # print(f"RESULT | AVG CPU UTIL: {avg_cpu}%")
+    # print(f"RESULT | MAX MEM UTIL: {ps_mem / (2**10)} KiB")
     # print(f"RESULT | END LOG SIZE: {log_size} B")
 
 
